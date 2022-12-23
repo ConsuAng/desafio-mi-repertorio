26d4d5a02dbf47a4c155494cd1f038f307d9ef90
Cristian Venegas <cristian.venegas@materoceano.com>
2022-12-22 21:55:41 -0300

    add : validations

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ app.get("/canciones", (req, res) => {
 app.post("/canciones", (req, res) => {
   try {
     const cancion = req.body;
+    console.log(cancion);
+
+    if (Object.values(cancion).some((value) => value === "")) {
+      return res.status(400).json({ message: "Falta completar un campo " });
+    }
+
     const canciones = JSON.parse(fs.readFileSync("repertorios.json", "utf8"));
     fs.writeFileSync(
       "repertorios.json",
@@ -41,9 +47,22 @@ app.put("/canciones/:id", (req, res) => {
   try {
     const { id } = req.params;
     const cancion = req.body;
+
+    if (Object.values(cancion).some((value) => value === "")) {
+      return res.status(400).json({ message: "Falta completar un campo " });
+    }
+
     const canciones = JSON.parse(fs.readFileSync("repertorios.json", "utf8"));
     const index = canciones.findIndex((cancion) => cancion.id === parseInt(id));
+    console.log(index);
+    if (index === -1) {
+      return res
+        .status(404)
+        .json({ message: "no se encuetra la cancion en la lista" });
+    }
+
     canciones[index] = cancion;
+
     fs.writeFileSync("repertorios.json", JSON.stringify(canciones));
     res.send("cancion actualizada");
   } catch (error) {
@@ -54,8 +73,18 @@ app.put("/canciones/:id", (req, res) => {
 app.delete("/canciones/:id", (req, res) => {
   try {
     const { id } = req.params;
+
     const canciones = JSON.parse(fs.readFileSync("repertorios.json", "utf8"));
     const index = canciones.findIndex((cancion) => cancion.id === parseInt(id));
+
+    console.log(index);
+
+    if (index === -1) {
+      return res.status(404).json({
+        message: "El recurso que desea eliminar no se encuetra en la lista",
+      });
+    }
+
     canciones.splice(index, 1);
     fs.writeFileSync("repertorios.json", JSON.stringify(canciones));
     res.send("cancion eliminada");
